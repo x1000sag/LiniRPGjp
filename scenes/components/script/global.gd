2491c96828b53648c661c78d229cf45896145510
x1000sag <x1000.sa@yandex.ru>
2024-01-29 23:35:29 +0300

    29.01 v1
    work with UI

---
+++ scenes/components/script/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+#очередь фраз
+@onready var queue : Array
+#теги для диалога(список фраз нужные для диалога)
+@onready var phrases = {
+	"test": ["_test_", "_test_item_"]
+}
+
--- scenes/components/script/init_window_component.gd
+++ scenes/components/script/init_window_component.gd
@@ -9,4 +9,12 @@ func _input(event):
 			Menu = preload("res://scenes/ui/esc_menu.tscn")
 			child=Menu.instantiate()	
 			get_parent().add_child(child)
-			
+	elif get_parent().has_node("Window_button"):
+		if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+			Menu = preload("res://scenes/ui/esc_menu.tscn")
+			child=Menu.instantiate()	
+			get_parent().add_child(child)
+			get_parent().get_node("Window_button").queue_free()
+	elif get_parent().has_node("Esc_Menu"):
+		if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+			get_parent().get_node("Esc_Menu").queue_free()
