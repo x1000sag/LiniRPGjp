5cb5efea3ff2d68855f3edda2d43d4503bc51eab
x1000sag <x1000.sa@yandex.ru>
2024-02-10 14:46:47 +0300

    10.02 v1
    DLOBAL_DIALOG script

--- scenes/components/script/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-#В РАБОТЕ! @x1000sa
-
-#очередь фраз
-@onready var queue : Array
-#теги для диалога(список фраз нужные для диалога)
-@onready var phrases = {
-	"test": ["_test_", "_test_item_"]
-}
--- scenes/ui/sripts/dialog.gd
+++ scenes/ui/sripts/dialog.gd
@@ -2,28 +2,22 @@ extends CanvasLayer
 
 @onready var text = $dialog/NinePatchRect/RichTextLabel
 @onready var dialog_ui = $dialog
-#очередь фраз
-@onready var queue : Array
-#теги для диалога(список фраз нужные для диалога)
-@onready var phrases = {
-	"test": ["_test_", "_test_item_"]
-}
 
 func start_dialog(dialog : String):
 	dialog_ui.visible = true
 #	добавление в очередь фразы по тегу
-	queue.assign(phrases[dialog])
+	GD.queue.assign(GD.phrases[dialog])
 #	Установка первой фразы
-	if queue.size() != 0:
-			text.text = queue[0]
-			queue.erase(text.text)
+	if GD.queue.size() != 0:
+			text.text = GD.queue[0]
+			GD.queue.erase(text.text)
 
 func _physics_process(delta):
 #	переход на следующую фразу
 	if Input.is_action_just_pressed("click") and dialog_ui.visible:
-		if queue.size() != 0:
-			text.text = queue[0]
-			queue.erase(text.text)
+		if GD.queue.size() != 0:
+			text.text = GD.queue[0]
+			GD.queue.erase(text.text)
 		else:
 			dialog_ui.visible = false
 			text.text = ""
