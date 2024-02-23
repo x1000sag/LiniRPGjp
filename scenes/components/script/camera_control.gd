37ca08bdc9e249ac59485702fdb0cd07c64404cd
x1000sag <x1000.sa@yandex.ru>
2024-02-23 21:00:25 +0300

    23.02 v1 FIX2

--- scenes/components/script/camera_control.gd
+++ scenes/components/script/camera_control.gd
@@ -3,15 +3,22 @@ extends Area2D
 var came_from:int
 
 func _on_body_entered(body):
+	#if %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == 1:
+		#%Camera.position.x += 1700
+		#came_from = 1
+	#elif %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == -1:
+		#came_from = -1
 	if %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == 1:
-		%Camera.position.x += 1700
 		came_from = 1
 	elif %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == -1:
 		came_from = -1
 
 func _on_body_exited(body):
-	if %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == -1:
-		%Camera.position.x -= 1700
-	elif %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == 1 and came_from == -1:
+	#if %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == -1 and came_from != 0:
+		#%Camera.position.x -= 1700
+	#came_from = 0
+	if came_from == 1 and %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == 1:
 		%Camera.position.x += 1700
-
+	elif came_from == -1 and %MainCharacter/AnimationPlayer/AnimationTree["parameters/idle/blend_position"] == -1:
+		%Camera.position.x -= 1700
+	came_from = 0
